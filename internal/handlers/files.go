318172836095869a769aa4ac5c02a1a2970d4be1
httpolar <http@polar.moe>
2024-05-13 22:04:11 +0400

    feat: moved user file save into separate functionand added mime type check

--- internal/handlers/files.go
+++ internal/handlers/files.go
@@ -6,20 +6,79 @@ import (
 	"imi/college/internal/models"
 	"imi/college/internal/writers"
 	"io"
+	"mime/multipart"
 	"net/http"
 	"os"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
-
-type FilesHandler struct {
-	db *gorm.DB
-}
 
 func NewFilesHandler(db *gorm.DB) *FilesHandler {
 	return &FilesHandler{db}
 }
 
+func IsSupportedImageType(file multipart.File) (bool, error) {
+	// only 512 bytes are needed to find out if file is an image
+	buf := make([]byte, 512)
+	if _, err := file.Read(buf); err != nil {
+		return false, err
+	}
+
+	if _, err := file.Seek(0, io.SeekStart); err != nil {
+		return false, err
+	}
+
+	switch http.DetectContentType(buf) {
+	case "image/png", "image/jpeg", "image/jpg":
+		return true, nil
+	default:
+		return false, nil
+	}
+}
+
+func SaveUserImage(image multipart.File, userID uuid.UUID, filename string) error {
+	baseDir := ".file-uploads"
+	userDir := fmt.Sprintf("%s/%s", baseDir, userID)
+
+	if _, err := image.Seek(0, io.SeekStart); err != nil {
+		return err
+	}
+
+	if _, err := os.Stat(baseDir); os.IsNotExist(err) {
+		os.Mkdir(baseDir, 0755)
+	} else if err != nil {
+		return err
+	}
+
+	if _, err := os.Stat(userDir); os.IsNotExist(err) {
+		os.Mkdir(userDir, 0755)
+	} else if err != nil {
+		return err
+	}
+
+	attachmentPath := fmt.Sprintf("%s/%s", userDir, filename)
+
+	out, err := os.Create(attachmentPath)
+	if err != nil {
+		return err
+	}
+
+	defer out.Close()
+
+	if _, err = io.Copy(out, image); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type FilesHandler struct {
+	db *gorm.DB
+}
+
+// This handler requires a request body to be a form
+// attachment - a file user attaches
 func (h *FilesHandler) CreateFile(w http.ResponseWriter, r *http.Request) error {
 	token, ok := r.Context().Value(contextkeys.TokenKey).(models.UserToken)
 	if !ok {
@@ -36,34 +95,21 @@ func (h *FilesHandler) CreateFile(w http.ResponseWriter, r *http.Request) error 
 		if _, ok := err.(*http.MaxBytesError); ok {
 			return TooLarge()
 		}
-
 		return err
 	}
 
 	defer attachment.Close()
 
-	baseDir := ".file-uploads"
-	userDir := fmt.Sprintf("%s/%s", baseDir, token.UserID)
-
-	if _, err := os.Stat(baseDir); os.IsNotExist(err) {
-		os.Mkdir(baseDir, 0755)
-	}
-
-	if _, err := os.Stat(userDir); os.IsNotExist(err) {
-		os.Mkdir(userDir, 0755)
-	}
-
-	attachmentPath := fmt.Sprintf("%s/%s", userDir, handler.Filename)
-
-	out, err := os.Create(attachmentPath)
+	isImage, err := IsSupportedImageType(attachment)
 	if err != nil {
 		return err
 	}
 
-	defer out.Close()
+	if !isImage {
+		return MalformedForm()
+	}
 
-	_, err = io.Copy(out, attachment)
-	if err != nil {
+	if err := SaveUserImage(attachment, token.UserID, handler.Filename); err != nil {
 		return err
 	}
 
