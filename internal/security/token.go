dbb7d0b0dc39e3e9d3747ec27f508e2541f06889
httpolar <dev@firefly.party>
2024-05-29 19:38:20 +0400

    feat: utility to extract bearer tokens out of http requests

--- internal/security/token.go
+++ internal/security/token.go
@@ -3,6 +3,9 @@ package security
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"errors"
+	"net/http"
+	"strings"
 )
 
 const DEFAULT_TOKEN_SIZE int = 72
@@ -15,4 +18,41 @@ func NewToken(size int) (string, error) {
 	}
 
 	return base64.StdEncoding.EncodeToString(bytes), nil
+}
+
+var ErrTokenNotFound error = errors.New("user token is empty or not found")
+
+// attempts to read auth token from the request's cookies
+// and from Authorzation header in case there's no cookie
+//
+// returned token string will be only the token itself
+// without any prefix, so "Bearer " part gets removed
+//
+// if there is no token or user provided invalid string
+// the error value will be set to ErrTokenNotFound
+func ExtractToken(r *http.Request) (string, error) {
+	var inputToken string
+
+	// attempt to read token from cookie first
+	if cookie, err := r.Cookie("token"); err == nil {
+		inputToken = cookie.Value
+	}
+
+	// if cookie wasn't read or empty attempt reading header
+	if header := r.Header.Get("Authorization"); len(header) > 0 && len(inputToken) == 0 {
+		inputToken = header
+	}
+
+	// if token is empty then there's no token
+	if len(inputToken) == 0 {
+		return "", ErrTokenNotFound
+	}
+
+	// make sure token has prefix and cut it
+	rawToken, isCut := strings.CutPrefix(inputToken, "Bearer ")
+	if !isCut {
+		return "", ErrTokenNotFound
+	}
+
+	return rawToken, nil
 }
