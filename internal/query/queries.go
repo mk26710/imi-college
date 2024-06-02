ba2dd033c5eff29878f6623d60b955d5cdcd5220
httpolar <dev@firefly.party>
2024-06-02 20:49:16 +0400

    feat: more shorthand queries

--- internal/query/queries.go
+++ internal/query/queries.go
@@ -7,6 +7,27 @@ import (
 	"gorm.io/gorm"
 )
 
-func GetUserByID(db *gorm.DB, dest *models.User, id uuid.UUID) error {
-	return db.Where(&models.User{ID: id}).First(dest).Error
+func ReadUserByID(db *gorm.DB, dest *models.User, id uuid.UUID) error {
+	return db.Where(&models.User{ID: id}).Joins("Details").First(dest).Error
+}
+
+func GetUserByID(db *gorm.DB, id uuid.UUID) (models.User, error) {
+	var user models.User
+
+	err := ReadUserByID(db, &user, id)
+	if err != nil {
+		return models.User{}, err
+	}
+
+	return user, nil
+}
+
+func GetTokenByValue(db *gorm.DB, value string) (models.UserToken, error) {
+	var token models.UserToken
+
+	if err := db.Where(&models.UserToken{Token: value}).First(&token).Error; err != nil {
+		return models.UserToken{}, err
+	}
+
+	return token, nil
 }
