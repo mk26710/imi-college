cd4c8d2850da0ba59ef0e9c77c42ca283b98db24
httpolar <dev@firefly.party>
2024-06-04 22:56:10 +0400

    feat: shorthand getting user addresses

--- internal/query/queries.go
+++ internal/query/queries.go
@@ -8,7 +8,8 @@ import (
 )
 
 func ReadUserByID(db *gorm.DB, dest *models.User, id uuid.UUID) error {
-	return db.Where(&models.User{ID: id}).Joins("Details").First(dest).Error
+	// todo: not sure about joining address, might be a bad idea
+	return db.Where(&models.User{ID: id}).Joins("Details").Joins("Address").First(dest).Error
 }
 
 func GetUserByID(db *gorm.DB, id uuid.UUID) (models.User, error) {
@@ -30,4 +31,14 @@ func GetTokenByValue(db *gorm.DB, value string) (models.UserToken, error) {
 	}
 
 	return token, nil
+}
+
+func GetUserAddressByUserID(db *gorm.DB, userID uuid.UUID) (models.UserAddress, error) {
+	var addr models.UserAddress
+
+	if err := db.Where(&models.UserAddress{UserID: userID}).Joins("Region").Joins("TownType").First(&addr).Error; err != nil {
+		return models.UserAddress{}, err
+	}
+
+	return addr, nil
 }
